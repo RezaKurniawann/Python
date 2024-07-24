f185c53cde8993e58aafe263af2414f0263b6a94
rezakurniawann <reza.kurniawan@gmail.com>
2024-07-24 12:36:34 +0700

    first commit

---
+++ variabel/variabel.py
@@ -0,0 +1,25 @@
+# variabel adalah tempat untuk menyimpan data
+
+# assignment nilai
+title = "Menghitung Luas Persegi Panjang : " # tipe data str
+p = 10 # tipe data int
+l = 5
+persegi_panjang = p * l
+
+print (title)
+print ("panjang = ", p) 
+print ("lebar = ", l) 
+print ("Luas Persegi panjang = ", persegi_panjang)
+
+print ("-------------------------")
+
+a = 7
+
+print ("Nilai a = ", a) 
+
+b = a
+
+print ("Nilai b = ", b)
+
+
+
