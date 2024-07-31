7833a9e1271804c7285bbb03645493ff03e88dff
rezakurniawann <reza.kurniawan0110@gmail.com>
2024-07-31 22:09:46 +0700

    import statement

---
+++ import statement/main.py
@@ -0,0 +1,15 @@
+# import
+
+# import statement is used to bring in modules, packages, or specific functions and variables from external libraries into current script
+
+import printProgram
+
+import dataProgram
+
+import functionProgram
+
+print ("my name is", dataProgram.name, "and i'm from", dataProgram.country )
+
+adder = functionProgram.plusFunction()
+result = adder (9, 10)
+print (result) # 19
