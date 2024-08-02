a2e09443244786b18eea64c54c0f4f631be1b2ae
rezakurniawann <reza.kurniawan0110@gmail.com>
2024-08-02 19:58:28 +0700

    module

---
+++ module/main.py
@@ -0,0 +1,22 @@
+import mathematics as m
+
+plus_result = m.plus (1, 2, 3, 4, 5)
+print(f"plus result : {plus_result}")
+
+minus_result = m.minus (100, 50)
+print(f"minus result : {minus_result}")
+
+times_result = m.times (5, 2)
+print(f"times result : {times_result}")
+
+devided_result = m.devided (10, 2, 5)
+print(f"devided result : {devided_result}") 
+
+add_powersofnumbers = m.powersofnumbers(2)
+powersofnumbers_result = add_powersofnumbers(4)
+print (f"Power of number result : {powersofnumbers_result}")
+
+
+
+
+
