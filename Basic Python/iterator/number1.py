39a647b5c26940877bc7e28ca80da4f16734ed96
RezaKurniawann <reza.kurniawan0110@gmail.com>
2025-01-06 21:23:41 +0700

    Iterator

---
+++ Basic Python/iterator/number1.py
@@ -0,0 +1,19 @@
+#create an iterator
+class MyNumber:
+    def __iter__(self):
+        self.a = 1
+        return self
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x 
+    
+myclass = MyNumber()
+myiter = iter (myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
