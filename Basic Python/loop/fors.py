154f10ea2e808af2a105fa1b5037495c181b3387
RezaKurniawann <reza.kurniawan0110@gmail.com>
2025-01-06 21:25:01 +0700

    Loop

---
+++ Basic Python/loop/fors.py
@@ -0,0 +1,66 @@
+# print each fruit in a fruit list
+fruitList = ["apple", "banana", "cherry"]
+for x in fruitList:
+    print(x)
+
+print ()
+for x in "banana":
+    print (x)
+
+print ()
+for x in fruitList:
+    print (x)
+    if x == "banana":
+        break
+
+#range 
+print ()
+for x in range (6):
+    print (x)
+
+print ()
+for x in range (2,6):
+    print (x)
+
+print ()
+for x in range (2,6):
+    print (x)
+
+print ()
+for  x in range (2, 30, 3):
+    print (x)
+
+#elese in for loop
+print ()
+for x in range (6):
+    print (x)
+else :
+    print ("Finish!!")
+
+print ()
+for x in range (6):
+    if x == 4:
+        break
+    print (x)
+else:
+    print ("Finishh") 
+
+#nested loop
+print ()
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
+#pass
+for x in [0, 1, 2]:
+    pass
+
+
+
+
+
+
+
